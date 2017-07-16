3aa9e2ad6b0903fa6deab4d445405751edc37ade
Jepson <547212806@qq.com>
2017-07-16 18:06:59 +0800

    UI组件引入：mint-UI 组件引入测试

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import Vue from 'vue'
+// 导入 Mint UI 模块 
+import MintUI from 'mint-ui'
+// 导入 Mint UI CSS
+import 'mint-ui/lib/style.css'
+// 按需引入
+// import { Button } from 'mint-ui';
+// Vue.component( Button.name, Button )
+// 如果项目中需要引入很多的 Mint-UI 的内容
+// 最简单的方式就是在 main.js 中全部引入
+Vue.use( MintUI )
+
+
+// 导入 组件
 import App from './App.vue'
 
 new Vue({
